6e6b99933be2661cff1a8a051fadd1d1a5290e3b
Leland Ede <lede@ncjfcj.org>
2021-02-11 16:04:53 -0800

    Working on the menu system pulling from api

---
+++ client/src/app/entities/menuItemEntity.ts
@@ -0,0 +1,5 @@
+export interface MenuItemEntity {
+  name: string;
+  link: string;
+  alias: string;
+}
--- client/src/app/menu/menu.component.ts
+++ client/src/app/menu/menu.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MenuItemEntity } from '../entities/menuItemEntity';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-menu',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.less']
 })
 export class MenuComponent implements OnInit {
+  public menuItems: MenuItemEntity[] = [];
+  private _menuSub: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    let url = environment.apiUrl + '/Menu';
+    // Creating a memory leak that needs to be fixed!
+    this._menuSub = this.http.get<MenuItemEntity[]>(url).subscribe(menu => {
+      this.menuItems = menu;
+    });
   }
 
 }
