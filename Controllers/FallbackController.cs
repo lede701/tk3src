cd759e0cfa2cea4667a72ce7aac1f926b9e48ac8
Leland Ede <lede@ncjfcj.org>
2021-02-24 16:05:01 -0800

    Adding vs instructions for code naming

--- Controllers/FallbackController.cs
+++ Controllers/FallbackController.cs
@@ -9,6 +9,26 @@ namespace tk3full.Controllers
 {
 	public class FallbackController : Controller
 	{
+		public FallbackController()
+		{
+			/**
+			 * Tools -> Options
+			 * Text Editor -> C# -> Code Style -> Naming
+			 * Manage naming styles
+			 * 
+			 * [+] -> _fieldName
+			 * Required Prefix: _
+			 * Capitalization: camelCaseName
+			 * Ok
+			 * 
+			 * [+] Add specification
+			 * Secification: Private or internal Field
+			 * Require Style: _fieldName
+			 * Servity: Suggestion
+			 * 
+			//*/
+		}
+
 		public ActionResult Index()
 		{
 			return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "index.html"), "text/HTML");
