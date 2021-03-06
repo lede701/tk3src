6167efca946efa0ada6673a1d7d77f696945d78f
Leland Ede <lede@zaxis-studios.com>
2021-03-06 08:36:44 -0800

    small chages to the menu

--- client/src/app/auth/auth-routing.module.ts
+++ client/src/app/auth/auth-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthCreateComponent } from './auth-create/auth-create.component';
 import { AuthComponent } from './auth.component';
 
 const routes: Routes = [
   { path: 'auth/:task', component: AuthComponent },
+  { path: 'auth/user/create', component: AuthCreateComponent }
 ];
 
 @NgModule({
